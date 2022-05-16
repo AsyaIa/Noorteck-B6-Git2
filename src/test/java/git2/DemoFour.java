ecddcb82adf4051118c7a19287de551433a5a27b
stiln <stiln@LAPTOP-NUEU3GPQ>
2022-05-16 18:56:16 -0400

    added stashes

--- src/test/java/git2/DemoFour.java
+++ src/test/java/git2/DemoFour.java
@@ -5,4 +5,13 @@ public class DemoFour {
 		System.out.println("Demo four...");
 	}
 
+	public static void div(int a, int b){
+		int result = a/b;
+		System.out.println(result);
+		}
+	
+	public static void add(int a, int b) {
+		int result = a+b;
+		System.out.println(result);
+	}
 }
